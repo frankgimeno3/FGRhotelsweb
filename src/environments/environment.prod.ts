478cd9059e472e771ee1779f3f9eb2460d4c2203
frank gimeno <frankgimeno3@gmail.com>
2024-01-04 19:58:01 +0100

    .

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBYImVknWsieOrvsuHOUh4Ghi_PSStaZqA",
+        authDomain: "fgrhoteles.firebaseapp.com",
+        projectId: "fgrhoteles",
+        storageBucket: "fgrhoteles.appspot.com",
+        messagingSenderId: "274987603561",
+        appId: "1:274987603561:web:d87396edd4887c6f4774f8",
+        measurementId: "G-369743TM6N"
+    },
+    production: true
+  };
